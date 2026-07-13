dd4fc489386d6693d75c73b92125de1bdcf85fd0
Vong Hoang Vinh <vonghoangvinh1989@gmail.com>
2026-07-12 19:27:40 -0600

    update code of linktree clone project

--- link_plant_project/link_plant/views.py
+++ link_plant_project/link_plant/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Profile, Link
 
@@ -15,3 +15,25 @@ class LinkCreateView(CreateView):
     fields = "__all__"
     success_url = reverse_lazy("link-list")
     # default templated called model_form -> link_form.html
+
+
+class LinkUpdateView(UpdateView):
+    model = Link
+    fields = ["text", "url"]
+    success_url = reverse_lazy("link-list")
+    # default templated called model_form -> link_form.html
+
+
+class LinkDeleteView(DeleteView):
+    model = Link
+    success_url = reverse_lazy("link-list")
+    # from to submit to delete the item
+    # expect a template name model_confirm_delete.html
+    # link_confirm_delete.html
+
+
+def profile_view(request, profile_slug):
+    profile = get_object_or_404(Profile, slug=profile_slug)
+    links = profile.links.all()
+    context = {"profile": profile, "links": links}
+    return render(request, "link_plant/profile.html", context)

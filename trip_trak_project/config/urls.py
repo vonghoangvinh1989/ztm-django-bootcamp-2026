c8adef45f246465f697354a9f1a5484a820c59a5
Vong Hoang Vinh <vonghoangvinh1989@gmail.com>
2026-07-18 19:35:33 -0600

    update code of register user

--- trip_trak_project/config/urls.py
+++ trip_trak_project/config/urls.py
@@ -19,11 +19,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import SignupView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("trip.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/signup/", SignupView.as_view(), name="signup"),
 ]
 
 if settings.DEBUG:
--- trip_trak_project/trip/views.py
+++ trip_trak_project/trip/views.py
@@ -10,6 +10,6 @@ class HomeView(TemplateView):
 
 
 def trips_list(request):
-    trips = Trip.objects.all()
+    trips = Trip.objects.filter(owner=request.user)
     context = {"trips": trips}
     return render(request, "trip/trip_list.html", context)
